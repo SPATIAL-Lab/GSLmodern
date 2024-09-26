c456663bd28c28060155bbedd873dc3446261419
Gabriel Bowen <gabe.bowen@utah.edu>
2024-09-25 19:13:58 -0600

    Add data and prep

---
+++ code/dataPrep.R
@@ -0,0 +1,23 @@
+
+sa = read.csv("data/shortAll.csv")
+lc = read.csv("data/longCarb.csv")
+lr = read.csv("data/longReservoir.csv")
+
+# US Pb210 data from Oliver et al., 2009
+pb.3510 = read.csv("data/pb210_3510.csv")
+
+
+
+lar = numeric()
+
+
+
+holes = unique(sa$Core)
+ci = match(sa$Core, holes)
+
+plot(lc$d13C.carb, lc$d18O.carb, pch = 16, col = "light grey", 
+     xlim = c(-3, 4))
+points(sa$d13C.carb[sa$Depth.min != 0], sa$d18O.carb[sa$Depth.min != 0],
+       pch = 21, bg = "light blue")
+points(sa$d13C.carb[sa$Depth.min == 0], sa$d18O.carb[sa$Depth.min == 0],
+       pch = 21, bg = "dark blue")
