3582a468a075c0422f459ff7dc8078079a6f9942
Gabriel Bowen <gabe.bowen@utah.edu>
2024-10-14 16:25:26 -0600

    Update add carbon plot

--- code/figure1.R
+++ code/figure1.R
@@ -7,6 +7,11 @@ qbg = rgb(qbg[1], qbg[2], qbg[3], alpha, maxColorValue = 255)
 lbg = col2rgb("coral2")
 lbg = rgb(lbg[1], lbg[2], lbg[3], alpha, maxColorValue = 255)
 
+# Age function
+reage = function(a){
+  return(sqrt(2007 - a))
+}
+
 # Plotting
 png("out/Fig1.png", 6, 8.3, "in", res = 600)
 layout(matrix(c(1, 2, 3), nrow = 3), 
@@ -14,83 +19,99 @@ layout(matrix(c(1, 2, 3), nrow = 3),
 
 ## Carbon isotopes ----
 par(mai = c(0, 0.7, 0.1, 0.7))
-plot(sa.DDQ$Age.med, sa.DDQ$d13C.carb, pch = 21, bg = "cadetblue3", 
-     xlim = rev(range(sa.DDQ$Age.med)), ylim = c(-1, 3.5), axes = FALSE,
+plot(sqrt(2007 - sa.DDQ$Age.med), 
+     sa.DDQ$d13C.carb, pch = 21, bg = "cadetblue3", 
+     xlim = reage(rev(range(sa.DDQ$Age.med))), ylim = c(-1, 3.5), axes = FALSE,
      xlab = "", ylab = "")
 mtext(expression(delta^{13}*"C"["carbonate"]*" (VPDB)"), 2, 3)
 axis(2)
-arrows(sa.DDQ$Age.025, sa.DDQ$d13C.carb, sa.DDQ$Age.975, sa.DDQ$d13C.carb,
+arrows(reage(sa.DDQ$Age.025), sa.DDQ$d13C.carb, 
+       reage(sa.DDQ$Age.975), sa.DDQ$d13C.carb,
        length = 0, lwd = 4, col = qbg)
-arrows(sa.DDQ$Age.med, sa.DDQ$d13C.carb + 2 * sa.DDQ$d13C.carb.sd, 
-       sa.DDQ$Age.med, sa.DDQ$d13C.carb - 2 * sa.DDQ$d13C.carb.sd,
+arrows(reage(sa.DDQ$Age.med), sa.DDQ$d13C.carb + 2 * sa.DDQ$d13C.carb.sd, 
+       reage(sa.DDQ$Age.med), sa.DDQ$d13C.carb - 2 * sa.DDQ$d13C.carb.sd,
        length = 0, lwd = 4, col = qbg)
 
-arrows(sa.DDL$Age.025, sa.DDL$d13C.carb, sa.DDL$Age.975, sa.DDL$d13C.carb,
+arrows(reage(sa.DDL$Age.025), sa.DDL$d13C.carb, 
+       reage(sa.DDL$Age.975), sa.DDL$d13C.carb,
        length = 0, lwd = 4, col = lbg)
-arrows(sa.DDL$Age.med, sa.DDL$d13C.carb + 2 * sa.DDL$d13C.carb.sd, 
-       sa.DDL$Age.med, sa.DDL$d13C.carb - 2 * sa.DDL$d13C.carb.sd,
+arrows(reage(sa.DDL$Age.med), sa.DDL$d13C.carb + 2 * sa.DDL$d13C.carb.sd, 
+       reage(sa.DDL$Age.med), sa.DDL$d13C.carb - 2 * sa.DDL$d13C.carb.sd,
        length = 0, lwd = 4, col = lbg)
 
-lines(sa.DDQ$Age.med, sa.DDQ$d13C.carb)
-lines(sa.DDL$Age.med, sa.DDL$d13C.carb)
+lines(reage(sa.DDQ$Age.med), sa.DDQ$d13C.carb)
+lines(reage(sa.DDL$Age.med), sa.DDL$d13C.carb)
 
-points(sa.DDQ$Age.med, sa.DDQ$d13C.carb, pch = 21, bg = "cadetblue3", cex = 1.5)
-points(sa.DDL$Age.med, sa.DDL$d13C.carb, pch = 21, bg = "coral2", cex = 1.5)
+points(reage(sa.DDQ$Age.med), sa.DDQ$d13C.carb, pch = 21, 
+       bg = "cadetblue3", cex = 1.5)
+points(reage(sa.DDL$Age.med), sa.DDL$d13C.carb, pch = 21, 
+       bg = "coral2", cex = 1.5)
 
-abline(v = 1847, lty = 3)
-abline(v = 1959, lty = 3)
+abline(v = reage(1847), lty = 3, col = "darkgoldenrod2")
+abline(v = reage(1959), lty = 3, col = "red3")
 
 ## Reservoir age ----
 par(mai = c(0, 0.7, 0, 0.7))
-plot(R.sum$Age.med, R.sum$R.med, pch = 21, bg = "cadetblue3", 
-     xlim = rev(range(sa.DDQ$Age.med)), ylim = range(R.sum[, 9:11]),
+plot(reage(R.sum$Age.med), R.sum$R.med, pch = 21, bg = "cadetblue3", 
+     xlim = reage(rev(range(sa.DDQ$Age.med))), ylim = range(R.sum[, 9:11]),
      axes = FALSE, xlab = "", ylab = "")
-mtext("Radiocarbon reservoir age", 4, 3)
+mtext(expression(""^{14}*"C reservoir age (years)"), 4, 3)
 axis(4)
-arrows(R.DDQ$Age.025, R.DDQ$R.med, R.DDQ$Age.975, R.DDQ$R.med,
+arrows(reage(R.DDQ$Age.025), R.DDQ$R.med, 
+       reage(R.DDQ$Age.975), R.DDQ$R.med,
        length = 0, lwd = 4, col = qbg)
-arrows(R.DDQ$Age.med, R.DDQ$R.025, R.DDQ$Age.med, R.DDQ$R.975,
+arrows(reage(R.DDQ$Age.med), R.DDQ$R.025, 
+       reage(R.DDQ$Age.med), R.DDQ$R.975,
        length = 0, lwd = 4, col = qbg)
 
-arrows(R.DDL$Age.025, R.DDL$R.med, R.DDL$Age.975, R.DDL$R.med,
+arrows(reage(R.DDL$Age.025), R.DDL$R.med, 
+       reage(R.DDL$Age.975), R.DDL$R.med,
        length = 0, lwd = 4, col = lbg)
-arrows(R.DDL$Age.med, R.DDL$R.025, R.DDL$Age.med, R.DDL$R.975,
+arrows(reage(R.DDL$Age.med), R.DDL$R.025, 
+       reage(R.DDL$Age.med), R.DDL$R.975,
        length = 0, lwd = 4, col = lbg)
 
-points(R.DDQ$Age.med, R.DDQ$R.med, pch = 21, bg = "cadetblue3", cex = 1.5)
-points(R.DDL$Age.med, R.DDL$R.med, pch = 21, bg = "coral2", cex = 1.5)
+points(reage(R.DDQ$Age.med), R.DDQ$R.med, pch = 21, 
+       bg = "cadetblue3", cex = 1.5)
+points(reage(R.DDL$Age.med), R.DDL$R.med, pch = 21, 
+       bg = "coral2", cex = 1.5)
 
-abline(v = 1847, lty = 3)
-abline(v = 1959, lty = 3)
+abline(v = reage(1847), lty = 3, col = "darkgoldenrod2")
+abline(v = reage(1959), lty = 3, col = "red3")
 
 ## Oxygen isotopes ----
 par(mai = c(0.7, 0.7, 0, 0.7))
-plot(sa.DDQ$Age.med, sa.DDQ$d18O.carb, pch = 21, bg = "cadetblue3", 
-     xlim = rev(range(sa.DDQ$Age.med)), ylim = c(-8, -4), axes = FALSE,
-     xlab = "", ylab = "")
+plot(reage(sa.DDQ$Age.med), sa.DDQ$d18O.carb, pch = 21, bg = "cadetblue3", 
+     xlim = reage(rev(range(sa.DDQ$Age.med))), ylim = c(-8, -4), 
+     axes = FALSE, xlab = "", ylab = "")
 mtext(expression(""^{210}*"Pb age (CE)"), 1, 3)
 mtext(expression(delta^{18}*"O"["carbonate"]*" (VPDB)"), 2, 3)
-axis(1)
+axis(1, at = reage(c(2000, 1800, 1500, 1000, 0)), 
+     labels = c(2000, 1800, 1500, 1000, 0))
 axis(2)
-arrows(sa.DDQ$Age.025, sa.DDQ$d18O.carb, sa.DDQ$Age.975, sa.DDQ$d18O.carb,
+arrows(reage(sa.DDQ$Age.025), sa.DDQ$d18O.carb, 
+       reage(sa.DDQ$Age.975), sa.DDQ$d18O.carb,
        length = 0, lwd = 4, col = qbg)
-arrows(sa.DDQ$Age.med, sa.DDQ$d18O.carb + 2 * sa.DDQ$d18O.carb.sd, 
-       sa.DDQ$Age.med, sa.DDQ$d18O.carb - 2 * sa.DDQ$d18O.carb.sd,
+arrows(reage(sa.DDQ$Age.med), sa.DDQ$d18O.carb + 2 * sa.DDQ$d18O.carb.sd, 
+       reage(sa.DDQ$Age.med), sa.DDQ$d18O.carb - 2 * sa.DDQ$d18O.carb.sd,
        length = 0, lwd = 4, col = qbg)
 
-arrows(sa.DDL$Age.025, sa.DDL$d18O.carb, sa.DDL$Age.975, sa.DDL$d18O.carb,
+arrows(reage(sa.DDL$Age.025), sa.DDL$d18O.carb, 
+       reage(sa.DDL$Age.975), sa.DDL$d18O.carb,
        length = 0, lwd = 4, col = lbg)
-arrows(sa.DDL$Age.med, sa.DDL$d18O.carb + 2 * sa.DDL$d18O.carb.sd, 
-       sa.DDL$Age.med, sa.DDL$d18O.carb - 2 * sa.DDL$d18O.carb.sd,
+arrows(reage(sa.DDL$Age.med), sa.DDL$d18O.carb + 2 * sa.DDL$d18O.carb.sd, 
+       reage(sa.DDL$Age.med), sa.DDL$d18O.carb - 2 * sa.DDL$d18O.carb.sd,
        length = 0, lwd = 4, col = lbg)
 
-lines(sa.DDQ$Age.med, sa.DDQ$d18O.carb)
-lines(sa.DDL$Age.med, sa.DDL$d18O.carb)
+lines(reage(sa.DDQ$Age.med), sa.DDQ$d18O.carb)
+lines(reage(sa.DDL$Age.med), sa.DDL$d18O.carb)
 
-points(sa.DDQ$Age.med, sa.DDQ$d18O.carb, pch = 21, bg = "cadetblue3", cex = 1.5)
-points(sa.DDL$Age.med, sa.DDL$d18O.carb, pch = 21, bg = "coral2", cex = 1.5)
+points(reage(sa.DDQ$Age.med), sa.DDQ$d18O.carb, pch = 21, 
+       bg = "cadetblue3", cex = 1.5)
+points(reage(sa.DDL$Age.med), sa.DDL$d18O.carb, pch = 21, 
+       bg = "coral2", cex = 1.5)
 
-abline(v = 1847, lty = 3)
-abline(v = 1959, lty = 3)
+abline(v = reage(1847), lty = 3, col = "darkgoldenrod2")
+abline(v = reage(1959), lty = 3, col = "red3")
 
 dev.off()
--- code/figure2.R
+++ code/figure2.R
@@ -1,26 +1,28 @@
 source("code/loadData.R")
-library(ks)
-
-# Bivariate kernal density for Holocene
-kd = kde(as.matrix(lc[c("d13C.carb", "d18O.carb")]))
-
-# Initialize plot
-png("out/Fig2.png", 6, 6, "in", res = 600)
-par(mar = c(5, 5, 1, 1))
-
-# Holocene data
-plot(lc$d13C.carb, lc$d18O.carb, pch = 16, col = "gray50", 
-     xlim = c(-3, 4), ylim = c(-9, -3), cex = 0.75, 
-     xlab = expression(delta^{13}*"C"[carb]),
-     ylab = expression(delta^{18}*"O"[carb]))
-plot(kd, cont = 95, drawlabels = FALSE, add = TRUE, lwd = 3, col = "gray50")
-
+source("code/helpers.R")
 
 # Load models
 source("code/models/Omodel.R")
 source("code/models/Cmodel.R")
+
 # Calcite precipitation temperature
 tc = 18
+
+# Bivariate kernal density for Holocene
+library(ks)
+kd = kde(as.matrix(lc[c("d13C.carb", "d18O.carb")]))
+
+# Initialize plot
+png("out/Fig2.png", 10, 5, "in", res = 600)
+layout(matrix(c(1, 2), ncol = 2))
+par(mar = c(5, 5, 1, 1))
+
+# Holocene data
+plot(lc$d13C.carb, lc$d18O.carb, pch = 16, col = "gray70", 
+     xlim = c(-3, 4), ylim = c(-9, -3), cex = 0.75, 
+     xlab = expression(delta^{13}*"C"[carb]),
+     ylab = expression(delta^{18}*"O"[carb]))
+plot(kd, cont = 95, drawlabels = FALSE, add = TRUE, lwd = 3, col = "gray70")
 
 # Model scenario 1
 ## Range of Fatm, terminal lake
@@ -42,23 +44,9 @@ O = Omod(EI, tc)
 C = Cmod(Fatm, tc)
 s2 = cbind(O, C)
 
-# Add cross-scenario connectors
-## Index for 10% Fatm intervals
-ri = match(seq(0.6, 0.9, by = 0.1), s1$Fatm) 
-
-## Plot them
-for(i in seq_along(ri)){
-  lines(c(s1$d13Cc[ri[i]], s2$d13Cc[ri[i]]),
-        c(s1$d18Oc[ri[i]], s2$d18Oc[ri[i]]), lty = 2)
-  text(s1$d13Cc[ri[i]], s1$d18Oc[ri[i]], paste("Fatm =", s1$Fatm[ri[i]]),
-       pos = 3, offset = 1.5)
-  text(s2$d13Cc[ri[i]], s2$d18Oc[ri[i]], paste("E/I =", s2$EI[ri[i]]),
-       adj = c(0, 2))
-}
-
 # Add each scenario to plot
-lines(s1$d13Cc, s1$d18Oc, lwd = 2, lty = 2)
-lines(s2$d13Cc, s2$d18Oc, lwd = 2, lty = 2)
+lines(s1$d13Cc, s1$d18Oc, lwd = 2)
+lines(s2$d13Cc, s2$d18Oc, lwd = 2)
 
 # Add CE data
 points(sa.DDL$d13C.carb, sa.DDL$d18O.carb,
@@ -80,5 +68,61 @@ points(sa.DDQ$d13C.carb[sa.DDQ$Age.med > 1957],
        sa.DDQ$d18O.carb[sa.DDQ$Age.med > 1957],
        pch = 21, bg = "red3", cex = 1.5)
 
-dev.off()
+arrows(4, -8, 4, -6, length = 0.1)
+text(4, -6.8, "+ E/I", pos = 2, srt = 90)
+arrows(4, -3.7, 2, -3.7, length = 0.1)
+text(3, -3.7, "+ Organic C", pos = 3)
 
+# Carbon panel
+## Add carbonate d13C at radiocarbon levels, convert to R to F14R
+lr = add.d13C(lr, lc)
+lr$F14R = exp(lr$R.v1 / -8033)
+
+sa.sub = sa.DDL[!is.na(sa.DDL$d13C.carb), ]
+R.DDL = add.d13C(R.DDL, sa.sub)
+R.DDL$F14R = exp(R.DDL$R.med / -8033)
+
+sa.sub = sa.DDQ[!is.na(sa.DDQ$d13C.carb), ]
+R.DDQ = add.d13C(R.DDQ, sa.sub)
+R.DDQ$F14R = exp(R.DDQ$R.med / -8033)
+
+## Plot
+par(mar = c(5, 5, 1, 1))
+plot(lr$F14R, lr$d13Cc, xlim = c(0.65, 1), ylim = c(-5, 5),
+     pch = 16, col = "gray70", cex = 0.75,
+     xlab = expression("F"^{14}*"R"),
+     ylab = expression(delta^{13}*"C"[carb]))
+
+points(R.DDL$F14R, R.DDL$d13Cc, pch = 21, bg = "palegreen3", cex = 1.5)
+points(R.DDQ$F14R, R.DDQ$d13Cc, pch = 21, bg = "palegreen3", cex = 1.5)
+
+points(R.DDL$F14R[R.DDL$Age.med > 1790], 
+       R.DDL$d13Cc[R.DDL$Age.med > 1790],
+       pch = 21, bg = "darkgoldenrod2", cex = 1.5)
+points(R.DDQ$F14R[R.DDQ$Age.med > 1790], 
+       R.DDQ$d13Cc[R.DDQ$Age.med > 1790],
+       pch = 21, bg = "darkgoldenrod2", cex = 1.5)
+
+points(R.DDL$F14R[R.DDL$Age.med > 1959], 
+       R.DDL$d13Cc[R.DDL$Age.med > 1959],
+       pch = 21, bg = "red3", cex = 1.5)
+points(R.DDQ$F14R[R.DDQ$Age.med > 1959], 
+       R.DDQ$d13Cc[R.DDQ$Age.med > 1959],
+       pch = 21, bg = "red3", cex = 1.5)
+
+d13Cc.atm = Cmod(1, tc)
+d13Cc.org = Cmod(0, tc)
+lines(c(0, 1), c(0, d13Cc.atm$d13Cc), lwd = 2)
+Forg = 0.5
+lines(c(0, 1), 
+      c(0, d13Cc.org$d13Cc * Forg + d13Cc.atm$d13Cc * (1 - Forg)), lwd = 2)
+lines(c(1, 1), c(d13Cc.atm$d13Cc, 
+                 d13Cc.org$d13Cc * Forg + d13Cc.atm$d13Cc * (1 - Forg)), 
+      lwd = 2)
+
+text(1, d13Cc.atm$d13Cc, "100% atm", adj = c(1.05, 2.2))
+text(1, d13Cc.org$d13Cc * Forg + d13Cc.atm$d13Cc * (1 - Forg), 
+     "50:50 atm:org", adj = c(1.05, -2.1))
+
+
+dev.off()
