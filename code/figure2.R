79242d3c696828597c07c84e75ca11d9fa64a4fc
Gabriel Bowen <gabe.bowen@utah.edu>
2025-03-25 15:17:53 -0600

    Update plots

--- code/figure2.R
+++ code/figure2.R
@@ -13,20 +13,20 @@ library(ks)
 kd = kde(as.matrix(lc[c("d13C.carb", "d18O.carb")]))
 
 # Initialize plot
-png("out/Fig2.png", 10, 5, "in", res = 600)
-layout(matrix(c(1, 2), ncol = 2))
+png("out/Fig2.png", 5, 10, "in", res = 600)
+layout(matrix(c(1, 2), nrow = 2))
 par(mar = c(5, 5, 1, 1))
 
 # Holocene data
 plot(lc$d13C.carb, lc$d18O.carb, pch = 16, col = "gray70", 
-     xlim = c(-3, 4), ylim = c(-9, -3), cex = 0.75, 
+     xlim = c(-5, 5), ylim = c(-9, -3), cex = 0.75, 
      xlab = expression(delta^{13}*"C"[carb]),
      ylab = expression(delta^{18}*"O"[carb]))
 plot(kd, cont = 95, drawlabels = FALSE, add = TRUE, lwd = 3, col = "gray70")
 
 # Model scenario 1
 ## Range of Fatm, terminal lake
-Fatm = seq(0.50, 1, by = 0.01)
+Fatm = seq(0.40, 1, by = 0.01)
 EI = rep(1, length(Fatm))
 
 ## Model
@@ -36,7 +36,7 @@ s1 = cbind(O, C)
 
 # Scenario 2
 ## Range of Fatm correlated with EI 
-Fatm = seq(0.50, 1, by = 0.01)
+Fatm = seq(0.40, 1, by = 0.01)
 EI = 1 - (1 - Fatm) * 1.8
 
 ## Model
@@ -50,23 +50,23 @@ lines(s2$d13Cc, s2$d18Oc, lwd = 2)
 
 # Add CE data
 points(sa.DDL$d13C.carb, sa.DDL$d18O.carb,
-       pch = 21, bg = "palegreen3", cex = 1.5)
+       pch = 21, bg = "palegreen3", cex = 1.5, lwd = 2)
 points(sa.DDQ$d13C.carb, sa.DDQ$d18O.carb,
-       pch = 21, bg = "palegreen3", cex = 1.5)
+       pch = 21, bg = "palegreen3", cex = 1.5, lwd = 2)
 
 points(sa.DDL$d13C.carb[sa.DDL$Age.med > 1790], 
        sa.DDL$d18O.carb[sa.DDL$Age.med > 1790],
-       pch = 21, bg = "darkgoldenrod2", cex = 1.5)
+       pch = 21, bg = "darkgoldenrod2", cex = 1.5, lwd = 2)
 points(sa.DDQ$d13C.carb[sa.DDQ$Age.med > 1790], 
        sa.DDQ$d18O.carb[sa.DDQ$Age.med > 1790],
-       pch = 21, bg = "darkgoldenrod2", cex = 1.5)
+       pch = 21, bg = "darkgoldenrod2", cex = 1.5, lwd = 2)
 
 points(sa.DDL$d13C.carb[sa.DDL$Age.med > 1957], 
        sa.DDL$d18O.carb[sa.DDL$Age.med > 1957],
-       pch = 21, bg = "red3", cex = 1.5)
+       pch = 21, bg = "red3", cex = 1.5, lwd = 2)
 points(sa.DDQ$d13C.carb[sa.DDQ$Age.med > 1957], 
        sa.DDQ$d18O.carb[sa.DDQ$Age.med > 1957],
-       pch = 21, bg = "red3", cex = 1.5)
+       pch = 21, bg = "red3", cex = 1.5, lwd = 2)
 
 arrows(4, -8, 4, -6, length = 0.1)
 text(4, -6.8, "+ E/I", pos = 2, srt = 90)
@@ -92,48 +92,49 @@ R.DDQ$F14R = exp(R.DDQ$R.med / -8033)
 
 ## Plot
 par(mar = c(5, 5, 1, 1))
-plot(lr$F14R, lr$d13Cc, xlim = c(0.65, 1), ylim = c(-5, 5),
+plot(lr$d13Cc, lr$F14R, xlim = c(-5, 5), ylim = c(0.65, 1), 
      pch = 16, col = "gray70", cex = 0.75,
-     xlab = expression("F"^{14}*"R"),
-     ylab = expression(delta^{13}*"C"[carb]))
+     xlab = expression(delta^{13}*"C"[carb]),
+     ylab = expression("F"^{14}*"R"))
 
-kd = kde(as.matrix(lr[c("F14R", "d13Cc")]))
+kd = kde(as.matrix(lr[c("d13Cc", "F14R")]))
 plot(kd, cont = 95, drawlabels = FALSE, add = TRUE, lwd = 3, col = "gray70")
 
-points(R.DDL$F14R, R.DDL$d13Cc, pch = 21, bg = "palegreen3", cex = 1.5)
-points(R.DDQ$F14R, R.DDQ$d13Cc, pch = 21, bg = "palegreen3", cex = 1.5)
+points(R.DDL$d13Cc, R.DDL$F14R, 
+       pch = 21, bg = "palegreen3", cex = 1.5, lwd = 2)
+points(R.DDQ$d13Cc, R.DDQ$F14R, 
+       pch = 21, bg = "palegreen3", cex = 1.5, lwd = 2)
 
-points(R.DDL$F14R[R.DDL$Age.med > 1790], 
-       R.DDL$d13Cc[R.DDL$Age.med > 1790],
-       pch = 21, bg = "darkgoldenrod2", cex = 1.5)
-points(R.DDQ$F14R[R.DDQ$Age.med > 1790], 
-       R.DDQ$d13Cc[R.DDQ$Age.med > 1790],
-       pch = 21, bg = "darkgoldenrod2", cex = 1.5)
+points(R.DDL$d13Cc[R.DDL$Age.med > 1790],
+       R.DDL$F14R[R.DDL$Age.med > 1790], 
+       pch = 21, bg = "darkgoldenrod2", cex = 1.5, lwd = 2)
+points(R.DDQ$d13Cc[R.DDQ$Age.med > 1790],
+       R.DDQ$F14R[R.DDQ$Age.med > 1790], 
+       pch = 21, bg = "darkgoldenrod2", cex = 1.5, lwd = 2)
 
-points(R.DDL$F14R[R.DDL$Age.med > 1959], 
-       R.DDL$d13Cc[R.DDL$Age.med > 1959],
-       pch = 21, bg = "red3", cex = 1.5)
-points(R.DDQ$F14R[R.DDQ$Age.med > 1959], 
-       R.DDQ$d13Cc[R.DDQ$Age.med > 1959],
-       pch = 21, bg = "red3", cex = 1.5)
+points(R.DDL$d13Cc[R.DDL$Age.med > 1959],
+       R.DDL$F14R[R.DDL$Age.med > 1959], 
+       pch = 21, bg = "red3", cex = 1.5, lwd = 2)
+points(R.DDQ$d13Cc[R.DDQ$Age.med > 1959],
+       R.DDQ$F14R[R.DDQ$Age.med > 1959], 
+       pch = 21, bg = "red3", cex = 1.5, lwd = 2)
 
 d13Cc.atm = Cmod(1, tc)
 d13Cc.org = Cmod(0, tc)
-lines(c(0, 1), c(0, d13Cc.atm$d13Cc), lwd = 2)
+lines(c(0, d13Cc.atm$d13Cc), c(0, 1), lwd = 2)
 Forg = 0.5
-lines(c(0, 1), 
-      c(0, d13Cc.org$d13Cc * Forg + d13Cc.atm$d13Cc * (1 - Forg)), lwd = 2)
-lines(c(1, 1), c(d13Cc.atm$d13Cc, 
-                 d13Cc.org$d13Cc * Forg + d13Cc.atm$d13Cc * (1 - Forg)), 
-      lwd = 2)
-points(1, d13Cc.atm$d13Cc, pch = 16)
-points(1, d13Cc.org$d13Cc * Forg + d13Cc.atm$d13Cc * (1 - Forg), pch = 16)
+lines(c(0, d13Cc.org$d13Cc * Forg + d13Cc.atm$d13Cc * (1 - Forg)),
+      c(0, 1), lwd = 2)
+lines(c(d13Cc.atm$d13Cc, d13Cc.org$d13Cc * Forg + d13Cc.atm$d13Cc * (1 - Forg)), 
+      c(1, 1), lwd = 2)
+points(d13Cc.atm$d13Cc, 1, pch = 16)
+points(d13Cc.org$d13Cc * Forg + d13Cc.atm$d13Cc * (1 - Forg), 1, pch = 16)
 
-text(1, d13Cc.atm$d13Cc, "100% atm", adj = c(1.05, 2.3))
-text(1, d13Cc.org$d13Cc * Forg + d13Cc.atm$d13Cc * (1 - Forg), 
-     "50:50 atm:org", adj = c(1.05, -1.8))
-text(0.68, 0, "100% rock", pos = 3)
-arrows(0.72, 0, 0.64, 0, length = 0.1)
+text(d13Cc.atm$d13Cc, 1, "100% atm", adj = c(1.1, 2.2))
+text(d13Cc.org$d13Cc * Forg + d13Cc.atm$d13Cc * (1 - Forg), 1, 
+     "50:50 atm:org", adj = c(-0.1, 2.2))
+text(0, 0.68, "100% rock", adj = c(0.5, 1.5), srt = 90)
+arrows(0, 0.72, 0, 0.64, length = 0.1)
 
 text(par("usr")[1] + diff(par("usr")[1:2]) * 0.05,
      par("usr")[4] - diff(par("usr")[3:4]) * 0.05,
